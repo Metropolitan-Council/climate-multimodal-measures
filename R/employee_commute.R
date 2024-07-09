2f5971d3fb2663dc966c06ff811fc747a6bcea20
Giesie <61886@icf.com>
2024-07-09 10:56:19 -0700

    changing to snake case

--- R/employee_commute.R
+++ R/employee_commute.R
@@ -1,34 +1,34 @@
-employeeCommute <- function(dailyCommuteNo, projectStart, projectLifetime, averageCommute) {
+employee_commute <- function(daily_commute_no, project_start, project_lifetime, average_commute) {
   
-  workingDays <- 260  # Assuming 260 working days per year
+  working_days <- 260  # Assuming 260 working days per year
   
   # Generate years project covers
-  projectYears <- seq(projectStart, projectStart + projectLifetime - 1)
+  project_years <- seq(project_start, project_start + project_lifetime - 1)
   
   # Initialize vectors to store results
-  vmtDisplaced <- numeric(length(projectYears))
-  ghgImpact <- numeric(length(projectYears))
+  vmt_displaced <- numeric(length(project_years))
+  ghg_impact <- numeric(length(project_years))
   
-  for (i in seq_along(projectYears)) {
-    year <- projectYears[i]
+  for (i in seq_along(project_years)) {
+    year <- project_years[i]
     
     # Calculate VMT displaced for the current year
-    vmtDisplacedYear <- dailyCommuteNo * averageCommute * workingDays
+    vmt_displaced_year <- daily_commute_no * average_commute * working_days
     
     # Filter GHG emission factor (EF) for the current year
-    greetEfYear <- GREETCarbonIntensity %>% filter(year == year)
+    greet_ef_year <- greet_carbon_intensity %>% filter(emission_year == year)
     
-    discountRate <- SocialCostCarbon %>% filter(year == year )
+    discount_rate <- social_cost_carbon %>% filter(emission_year == year)
     
     # Calculate GHG impact for the current year
-    ghgImpactYear <- vmtDisplacedYear * greetEfYear$EF * discountRate
+    ghg_impact_year <- vmt_displaced_year * greet_ef_year$ef * discount_rate
     
     # Store results for the current year
-    vmtDisplaced[i] <- vmtDisplacedYear
-    ghgImpact[i] <- ghgImpactYear
+    vmt_displaced[i] <- vmt_displaced_year
+    ghg_impact[i] <- ghg_impact_year
   }
   
   # Returning the results as a data frame
-  return(data.frame(year = projectYears, vmtDisplaced = vmtDisplaced, ghgImpact = ghgImpact))
+  return(data.frame(year = project_years, vmt_displaced = vmt_displaced, ghg_impact = ghg_impact))
 }
 
