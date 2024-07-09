0313deb1d034cadad8e2cea026eb0b7c61368765
Giesie <61886@icf.com>
2024-07-09 11:46:40 -0700

    Updated function to depend on EF year and added test

--- R/employee_commute.R
+++ R/employee_commute.R
@@ -5,6 +5,8 @@ employee_commute <- function(daily_commute_no, project_start, project_lifetime, 
   # Generate years project covers
   project_years <- seq(project_start, project_start + project_lifetime - 1)
   
+  print(project_lifetime)
+  
   # Initialize vectors to store results
   vmt_displaced <- numeric(length(project_years))
   ghg_impact <- numeric(length(project_years))
@@ -16,12 +18,12 @@ employee_commute <- function(daily_commute_no, project_start, project_lifetime, 
     vmt_displaced_year <- daily_commute_no * average_commute * working_days
     
     # Filter GHG emission factor (EF) for the current year
-    greet_ef_year <- greet_carbon_intensity %>% filter(emission_year == year)
+    greet_ef_year <- GREETCarbonIntensity %>% filter(Year == year)
     
-    discount_rate <- social_cost_carbon %>% filter(emission_year == year)
+    discount_rate <- SocialCostCarbon %>% filter(`emission.year` == year & gas == "CO2")
     
     # Calculate GHG impact for the current year
-    ghg_impact_year <- vmt_displaced_year * greet_ef_year$ef * discount_rate
+    ghg_impact_year <- vmt_displaced_year * greet_ef_year$gasoline * discount_rate$`2.5% Ramsey`
     
     # Store results for the current year
     vmt_displaced[i] <- vmt_displaced_year
@@ -32,3 +34,4 @@ employee_commute <- function(daily_commute_no, project_start, project_lifetime, 
   return(data.frame(year = project_years, vmt_displaced = vmt_displaced, ghg_impact = ghg_impact))
 }
 
+#test <- employee_commute(200, 2025, 10, 2.8)
