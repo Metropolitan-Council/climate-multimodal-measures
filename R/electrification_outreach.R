a9ffe3f84b24888baf7ed02f9e9d4001fc492d46
Giesie <61886@icf.com>
2024-07-26 10:47:53 -0700

    electrification outreach first go

---
+++ R/electrification_outreach.R
@@ -0,0 +1,55 @@
+electrification_outreach <- function(no_participants, conversion_rate, project_start, project_lifetime) {
+  average_annual_accrual <- 5567
+  
+  # Generate years project covers
+  project_years <- seq(project_start, project_start + project_lifetime - 1)
+  
+  # Initialize vectors to store results
+  auto_vmt_displaced <- numeric(length(project_years))
+  ghg_impact <- numeric(length(project_years))
+  carbon_cost <- numeric(length(project_years))
+  
+  for (i in seq_along(project_years)) {
+    year <- project_years[i]
+    
+    # Calculate VMT displaced for the current year
+    vmt_displaced_year <- no_participants * conversion_rate * average_annual_accrual
+    
+    # Filter GHG emission factor (EF) for the current year
+    greet_ef_year <- GREETCarbonIntensity %>% filter(Year == year)
+    
+    discount_rate <- SocialCostCarbon %>% filter(`emission.year` == year & gas == "CO2")
+    
+    fleet_proportion <- FleetProportion %>% filter(Year == year)
+    
+    # Calculate GHG impact for the current year
+    ghg_impact_year <-
+      ((vmt_displaced_year * greet_ef_year$gasoline * fleet_proportion$gasoline) +
+         (vmt_displaced_year * greet_ef_year$diesel * fleet_proportion$diesel) +
+         (vmt_displaced_year * greet_ef_year$compressed_natural_gas * fleet_proportion$compressed_natural_gas) +
+         (vmt_displaced_year * greet_ef_year$electricity * fleet_proportion$electricity))
+    
+    social_cost_carbon <- ghg_impact_year * discount_rate$`2.0% Ramsey`
+    
+    # Store results for the current year
+    auto_vmt_displaced[i] <- vmt_displaced_year
+    ghg_impact[i] <- ghg_impact_year
+    carbon_cost[i] <- social_cost_carbon
+  }
+  
+  # Calculate total vmt_displaced and ghg_impact
+  total_vmt_displaced <- sum(auto_vmt_displaced)
+  total_ghg_impact <- sum(ghg_impact)
+  total_carbon_cost <- sum(carbon_cost)
+  
+  # Create a data frame with results including totals
+  results <- data.frame(year = c(project_years, "Total"),
+                        vmt_displaced = c(auto_vmt_displaced, total_vmt_displaced),
+                        ghg_impact = c(ghg_impact, total_ghg_impact),
+                        carbon_cost = c(carbon_cost, total_carbon_cost))
+  
+  return(results)
+}
+
+# Example usage
+test <- electrification_outreach(4000, .04, 2025, 5)
