68bdf54072995baba7659cb1639ed08a1a117867
Giesie <61886@icf.com>
2024-10-28 12:36:43 -0400

    psuedo code roadway functions

--- R Scripts/corridor_speed_improvement.R
+++ R Scripts/corridor_speed_improvement.R
@@ -1,1 +1,8 @@
 # corridor_speed_improvement
+# Fuel Consumption Reduced (Gallons) = Corridor Distance (miles) × AADTno build [k1speed no build – (1 + EID) × k1speed build]
+# Where:
+# •	AADTno build = Annual average daily traffic under the no-build condition
+# •	EID = Elasticity of induced VMT due to improved corridor speed; default value can be found in Table 22, with justification further explained the following section.
+# •	K1 = running fuel use factor, in gallon/hour. The default assumes LD gasoline vehicles only: 000019137 × cruise-speed2 -0.0020660× cruise-speed +0.088916
+# GHG Reduced (kg) = Fuel Consumption Reduced (Gallons) * 9.915
+
--- R Scripts/intersection_delay_reductions.R
+++ R Scripts/intersection_delay_reductions.R
@@ -1,1 +1,5 @@
 # intersection_delay_reductions
+# Fuel Consumption Reduced (Gallons) = k2 * Total Peak Hour Delay Reduced
+# Where:
+#   •	k2 = idling fuel use factor, in gallon/hour. The default value is 0.37. Since more than 94% of the U.S. on-road fleet is light-duty, the default value was estimated using light-duty only, with a 45/55 split between gasoline compact sedan and gasoline large sedan from Table 17.  The average of no-load and with-load factors were used to calculate the default. 
+# GHG Reduced (kg) = Fuel Consumption Reduced (Gallons) * 9.915
