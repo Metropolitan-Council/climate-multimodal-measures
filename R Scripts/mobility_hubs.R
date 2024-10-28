f6244964207a1265a9b4d4ebfcff094aede5912b
Giesie <61886@icf.com>
2024-10-28 12:36:22 -0400

    mobility hubs quantification

--- R Scripts/mobility_hubs.R
+++ R Scripts/mobility_hubs.R
@@ -1,1 +1,91 @@
-# mobility_hubs 
+mobility_hubs <-
+  function(mobility_mode,
+           population_3mile,
+           added_vmt
+           project_lifetime,
+           project_start,
+           location,
+           reduction_potential = NULL,
+           annual_vmt = NULL) {
+    if (is.null(reduction_potential)) {
+      reduction_potential <- TotalVMTReductionPotential %>% filter(mobility_mode == fleet) %>% pull(total_vmt_reduction_potential)
+    }
+    
+    if (is.null(annual_vmt)) {
+      annual_vmt = 5567
+    }
+    # Generate years project covers
+    project_start <- lubridate::year(project_start)
+    project_start <- as.numeric(project_start)
+    project_years <-
+      seq(project_start, project_start + project_lifetime - 1)
+    
+    # Initialize vectors to store results
+    auto_vmt_displaced <- numeric(length(project_years))
+    ghg_impact <- numeric(length(project_years))
+    carbon_cost <- numeric(length(project_years))
+    
+    for (i in seq_along(project_years)) {
+      current_year <- project_years[i]
+      
+      # Calculate auto VMT displaced
+      vmt_displaced_year <- reduction_potential * population_3mile * annual_vmt
+      
+      # Calculate GHG impact
+      greet_ef_year <- GREETCarbonIntensity %>% filter(Year == current_year)
+      
+      # Filter to CTU provided
+      FleetData <- FleetData %>% filter(ctu == location)
+      
+      FleetData <- FleetData %>% mutate(year = as.numeric(year))
+      
+      # Determine the closest year
+      closest_year <- FleetData %>%
+        summarise(closest_year = year[which.min(abs(year - current_year))]) %>%
+        pull(closest_year)
+      
+      # Filter the data set to get the fleet proportions from the closest year
+      fleet_proportion <- FleetData %>%
+        filter(year == closest_year)
+      
+      ghg_impact_year <- (((
+        vmt_displaced_year * greet_ef_year$gasoline * fleet_proportion$gasoline
+      ) +
+        (
+          vmt_displaced_year * greet_ef_year$diesel * fleet_proportion$diesel
+        ) +
+        (
+          vmt_displaced_year * greet_ef_year$electricity * fleet_proportion$electricity
+        )
+      ) -
+        (added_vmt * greet_ef_year$diesel)) / 1000000
+      
+      #################################################################################################
+      
+      # Calculate social cost of carbon for the current year
+      discount_rate <-
+        SocialCostCarbon %>% filter(`emission.year` == current_year &
+                                      gas == "CO2")
+      social_cost_carbon <-
+        ghg_impact_year * discount_rate$`2.0% Ramsey`
+      # Store results for the current year
+      auto_vmt_displaced[i] <- vmt_displaced_year
+      ghg_impact[i] <- ghg_impact_year
+      carbon_cost[i] <- social_cost_carbon
+    }
+    
+    # Calculate total vmt_displaced and ghg_impact
+    total_vmt_displaced <- sum(auto_vmt_displaced)
+    total_ghg_impact <- sum(ghg_impact)
+    total_carbon_cost <- sum(carbon_cost)
+    
+    # Create a data frame with results including totals
+    results <- data.frame(
+      year = c(project_years, "Total"),
+      vmt_displaced = c(auto_vmt_displaced, total_vmt_displaced),
+      ghg_impact = c(ghg_impact, total_ghg_impact),
+      carbon_cost = c(carbon_cost, total_carbon_cost)
+    )
+    
+    return(results)
+  }
