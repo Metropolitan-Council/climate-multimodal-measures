a9965efb4edd4ed27c076041a847022ba4fc759c
Giesie <61886@icf.com>
2024-11-05 18:11:11 -0500

    added vehicle population estimate

---
+++ data/vehicle_population_community_type.R
@@ -0,0 +1,9 @@
+VehiclePopulationCTU <- VehiclePopulation %>%
+  filter(var == "TotStock", mode == "PLDV", year == 2025) %>%
+  group_by(ctu, year) %>% rename(CTU_NAME = ctu)
+
+VehiclePopulationCommunityType <- VehiclePopulationCTU %>%
+  left_join(CommunityTypeShape)  %>%
+  filter(!is.na(year), !is.na(MappedCommunity), !is.na(value)) %>%
+  group_by(year, MappedCommunity) %>%
+  summarise(population = mean(value))
